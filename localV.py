42ae1143ee0e66ffb3372eed0e165b790762efff
veropetit <83323003+veropetit@users.noreply.github.com>
2022-09-21 15:27:56 -0400

    Add docstring to local_out

--- localV.py
+++ localV.py
@@ -24,7 +24,25 @@ def interpol_lin(v, x, u):
 
 
 def unno_local_out_IV(ub, ua, pattern, small_u, w, all_u, sin_theta, cos_theta, sin_2chi, cos_2chi, kappa, bnu, mu):
+    '''
+    Calculate a local line profile in all Stokes paramters.
+    
+    :param ub: the lambda_B in doppler width units.
+    :param ua: local radial velocity in doppler width units.
+    :param pattern: a zeeman pattern dictionary returned by the zeeman_pattern function
+    :param small_u: the uo array for the w array that contains the centered line profile function
+    :param w: the line profile function centered at uo=0, calculated over small_u (the profile functions are shifted and interpolated for the addition of all the zeeman components, instead of recalculated, to same computation time).
+    :param all_u: the full wavelength array in doppler width units. It need to be larger enough to accomodate the largest zeeman and rotational shifts.
+    :param sin_theta: sin of the angle between the local magnetic field and the LOS.
+    :param cos_theta: cos of the angle between the local magnetic field and the LOS.
+    :param sin_2chi: sin of 2x the angle between the local magnetic field and the reference direction in the plane of the sky
+    :param cos_2chi: cos of 2x the angle between the local magnetic field and the reference direction in the plane of the sky
+    :param kappa: the line strength parameter
+    :param bnu: the slope of the source function with respect to the vertical optical depth
+    :param mu: the angle between the normal to the surface and the LOS. 
+    
 
+    '''
 
     eta_rho = np.zeros( all_u.size,
                           dtype=[('pi', complex), ('sigma_r', complex),('sigma_b', complex) ]  )
