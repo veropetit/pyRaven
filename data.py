3d2c4a82a314418fbf2dd24d27c48558677cc12a
veropetit <83323003+veropetit@users.noreply.github.com>
2023-05-22 21:33:28 -0400

    Add the modification to the read_packet function
    move the creation to the data packet to its own function, so that I can use the creator in the read function.

--- data.py
+++ data.py
@@ -5,6 +5,8 @@ import specpolFlow as pol
 import copy
 import matplotlib.pyplot as plt
 
+# written by Robin Moore, Veronique Petit, and Patrick Stanley
+
 # class for a collection of LSD profiles 
 # with meta information for the star, 
 # and also for each individual LSD profiles.
@@ -32,40 +34,39 @@ class LSDprofs:
     """
     self.lsds = lsds
 
-  def write(self, f, name):
+  def write(self, f, obs_names):
     """
     Helper function that writes the contents of an LSDProfs object to an h5 file
 
     :param f: h5 file being written
     :param lsds: LSDprofs object being stored in the file
-    :param name: name of the LSDprofs object
+    :param obs_names: (List of string) the IDs to be used for each observations
 
     :rtype: none
     """
 
-    def write_lsd(lsd, f, name):
+    def write_lsd(lsd, f):
       """
       Helper function that writes the contents of an lsd_prof to an h5 file
 
       :param f: h5 file being written
       :param lsd: lsd_prof being stored in the file
-      :param name: name of the LSDprofs object and the index of the lsd_prof in the LSDprofs' list.
 
       :rtype: none
       """
-      f.create_dataset(name + '_vel', data = lsd.vel)
-      f.create_dataset(name + '_specI', data = lsd.specI)
-      f.create_dataset(name + '_specSigI', data = lsd.specSigI)
-      f.create_dataset(name + '_specV', data = lsd.specV)
-      f.create_dataset(name + '_specSigV', data = lsd.specSigV)
-      f.create_dataset(name + '_specN1', data = lsd.specN1)
-      f.create_dataset(name + '_specSigN1', data = lsd.specSigN1)
-      f.create_dataset(name + '_specN2', data = lsd.specN2)
-      f.create_dataset(name + '_specSigN2', data = lsd.specSigN2)
-      f.create_dataset(name + '_header', data = lsd.header)
+      f.create_dataset('vel', data = lsd.vel)
+      f.create_dataset('specI', data = lsd.specI)
+      f.create_dataset('specSigI', data = lsd.specSigI)
+      f.create_dataset('specV', data = lsd.specV)
+      f.create_dataset('specSigV', data = lsd.specSigV)
+      f.create_dataset('specN1', data = lsd.specN1)
+      f.create_dataset('specSigN1', data = lsd.specSigN1)
+      f.create_dataset('specN2', data = lsd.specN2)
+      f.create_dataset('specSigN2', data = lsd.specSigN2)
+      f.create_dataset('header', data = lsd.header)
 
     for i in range(0, len(self.lsds)):
-      write_lsd(self.lsds[i], f, name + '_' + str(i))
+      write_lsd(self.lsds[i], f.create_group(obs_names[i]))
 
   def scale(self, vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav):
 
@@ -120,10 +121,10 @@ class DataPacket:
   scaled:
   cutfit:
   """
-  def __init__(self, star_name, nobs, fname,
+  def __init__(self, star_name, nobs, obs_names,
               vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav,
               fitrange, vsini,
-              original):
+              original, scaled, cutfit):
     """
     Initialization method for DataPacket
     :param self: The object being initialized
@@ -138,7 +139,7 @@ class DataPacket:
 
     self.star_name = star_name
     self.nobs = nobs
-    self.fname = fname
+    self.obs_names = obs_names
     self.vrad = vrad
     self.Ic = Ic
     self.wint_data = wint_data
@@ -149,9 +150,8 @@ class DataPacket:
     self.vsini = vsini
 
     self.original = original
-    self.scaled = original.scale(vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav)
-    self.cutfit = self.scaled.cut(fitrange)
-
+    self.scaled = scaled
+    self.cutfit = cutfit
 
   def write(self, fname):
     """
@@ -166,21 +166,28 @@ class DataPacket:
     with h5py.File(fname,'w') as f:
       f.create_dataset('star_name', data = self.star_name)
       f.create_dataset('nobs', data = self.nobs)
-      f.create_dataset('fname', data = self.fname)
+      f.create_dataset('obs_names', data = self.obs_names)
       f.create_dataset('vrad', data = self.vrad)
       f.create_dataset('Ic', data = self.Ic)
-      f.create_dataset('wint_data', data='wint_data')
-      f.create_dataset('wpol_data', data='wpol_data')
+      f.create_dataset('wint_data', data=self.wint_data)
+      f.create_dataset('wpol_data', data=self.wpol_data)
+      f.create_dataset('wint_rav', data=self.wint_data)
+      f.create_dataset('wpol_rav', data=self.wpol_data)
 
       f.create_dataset('fitrange', data = self.fitrange)
       f.create_dataset('vsini', data = self.vsini)
 
-      self.original.write(f, 'original')
-      self.scaled.write(f, 'scaled')
-      self.cutfit.write(f, 'cutfit')
+      f_original = f.create_group('original')
+      self.original.write(f_original, self.obs_names)
+      
+      f_scaled = f.create_group('scaled') 
+      self.scaled.write(f_scaled, self.obs_names)
+
+      f_cutfit = f.create_group('cutfit')
+      self.cutfit.write(f_cutfit, self.obs_names)
 
   def plotI(self):
-    # Patrick Stanley
+    # Written by Patrick Stanley
 
     fig, ax = plt.subplots(1,3, figsize=(30,10))
 
@@ -241,13 +248,47 @@ class DataPacket:
     return(fig, ax)
 
 def read_lsds_from_sfiles(fnames):
+  '''
+  Function to create a LSDprof object from a list of filenames for LSD profiles in the .s format
+  '''
   lsds = []
   for fname in fnames:
     lsds.append(pol.iolsd.read_lsd(fname))
   return(LSDprofs(lsds))
 
 
-## I will also need a read method to get a h5 file back into a DataPacket object.
+def create_Packet(star_name, nobs, obs_names,
+            vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav,
+            fitrange, vsini,
+            original):
+  '''
+  Create a new DataPacket from meta information and a LSDprof object with the original LSD profiles. 
+  The creator function will calculate the scaled (radial velocity correction, normalization, LSD weigth scaling) version of the LSD profiles
+  based on the meta information provided. 
+
+  :param star_name: (string) the name (or ID) for the star
+  :param nobs: (int) the number of LSD profiles to be analyzed together
+  :param obs_names: (list of strings) ID for the observations to be used in e.g. graphs (e.g. filename, obs number, etc)
+  :param vrad: (1D array) radial velocity for the LSD profiles to put them in the stellar reference frame (if the LSD profiles are already in the star reference frame, just put an array of zeros)
+  :param Ic: (1D array) continuum normalization values for the LSD profiles (if the LSD profiles are already normalized, just input an array of ones)
+  :param wint_data: (1D array) the intensity weigth that was used to calculate the LSD profiles
+  :param wpol_data: (1D array) the polarization weigth that was used to calculate the LSD profiles
+  :param wint_rav: (float) the common LSD intensity weigth that will be used for all of the observations
+  :param wpol_rav: (float) the common LSD intensity weigth that will be used for all of the observations
+  :param fitrange: (float) the velocity range around the line center that will be use for the fitting
+  :param vsini: (float) the vsini of the star (RIGHT NOW, ONLY FOR DISPLAY PURPOSES)
+  :param original: (LSDprofs object) the original LSD profiles
+  '''
+  
+  scaled = original.scale(vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav)
+  cutfit = scaled.cut(fitrange)
+
+  Packet = DataPacket(star_name, nobs, obs_names,
+            vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav,
+            fitrange, vsini,
+            original, scaled, cutfit)
+
+  return(Packet)
 
 def read_packet(fname):
   """
@@ -258,40 +299,42 @@ def read_packet(fname):
   :rtype DataPacket:
   """
   with h5py.File(fname) as f:
-    fitrange = (f['fitrange'])[()]
-    vsini = (f['vsini'])[()]
+    nobs = (f['nobs'])[()]
+    star_name = (f['star_name'])[()]
+    obs_names = (f['obs_names'])[()]
+    vrad = (f['vrad'])[()]
+    Ic = (f['Ic'])[()]
     wint_data = (f['wint_data'])[()]
     wpol_data = (f['wpol_data'])[()]
     wint_rav = (f['wint_rav'])[()]
-    wpol_rav = (f['wpol_data'])[()]
-    original = read_lsds(f, 'original')
-    scaled = read_lsds(f, 'scaled')
-    cutfit = read_lsds(f, 'cutfit')
-    return DataPacket(fitrange, vsini,
-                      wint_data, wpol_data, wint_rav, wpol_rav,
-                      original, scaled, cutfit)
+    wpol_rav = (f['wpol_rav'])[()]
+    fitrange = (f['fitrange'])[()]
+    vsini = (f['vsini'])[()]
+    original = read_lsds(f['original'], obs_names)
+    scaled = read_lsds(f['scaled'], obs_names)
+    cutfit = read_lsds(f['cutfit'], obs_names)
+    return DataPacket(star_name, nobs, obs_names,
+              vrad, Ic, wint_data, wpol_data, wint_rav, wpol_rav,
+              fitrange, vsini,
+              original, scaled, cutfit)
     
-def read_lsds(f, name):
+def read_lsds(f, obs_names):
   """
   Helper function that reads an LSDprofs object from an h5 file
 
   :param f: the h5 file with the data
-  :param name: name of the LSDprof object
+  :param name: name of the LSDprof object (e.g. original, scaled, or cutfit)
+  :param nobs: the number of LSD profiles in the LSDprof object. 
 
   :rtype LSDprof:
   """
-  nobs = (f[name+'_nobs'])[()]
-  star_name = (f[name+'_star_name'])[()]
-  fname = (f[name+'_fname'])[()]
-  vrad = (f[name+'_vrad'])[()]
-  Ic = (f[name+'_Ic'])[()]
   lsds = []
-  for i in range(0, nobs):
-    lsds.append(read_lsd(f, name + '_' + str(i)))
-  lsd_profs = LSDprofs(star_name, nobs, fname, vrad, Ic, lsds)
+  for obsname in obs_names:
+    lsds.append(read_lsd(f[obsname]))
+  lsd_profs = LSDprofs(lsds)
   return lsd_profs
 
-def read_lsd(f, name):
+def read_lsd(f):
   """
   Helper function that reads an lsd_prof from an h5 file
 
@@ -300,16 +343,16 @@ def read_lsd(f, name):
 
   :rtype lsd_prof:
   """
-  vel = np.array(f[name+'_vel'])
-  specI = np.array(f[name+'_specI'])
-  specSigI = np.array(f[name+'_specSigI'])
-  specV = np.array(f[name+'_specV'])
-  specSigV = np.array(f[name+'_specSigV'])
-  specN1 = np.array(f[name+'_specN1'])
-  specSigN1 = np.array(f[name+'_specSigN1'])
-  specN2 = np.array(f[name+'_specN2'])
-  specSigN2 = np.array(f[name+'_specSigN2'])
-  header = (f[name+'_header'])[()]
+  vel = np.array(f['vel'])
+  specI = np.array(f['specI'])
+  specSigI = np.array(f['specSigI'])
+  specV = np.array(f['specV'])
+  specSigV = np.array(f['specSigV'])
+  specN1 = np.array(f['specN1'])
+  specSigN1 = np.array(f['specSigN1'])
+  specN2 = np.array(f['specN2'])
+  specSigN2 = np.array(f['specSigN2'])
+  header = (f['header'])[()]
   lsd = pol.iolsd.lsd_prof(vel, specI, specSigI, specV, specSigV, specN1, specSigN1, specN2=specN2, specSigN2=specSigN2, header=header)
   return lsd
 
