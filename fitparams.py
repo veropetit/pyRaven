36a17de2119cca821ff4b019ad3a5e3940967d29
pjstanley1 <pjstanley5391@gmail.com>
2023-08-16 06:42:45 -0400

    added fit with fixed vsini

--- fitparams.py
+++ fitparams.py
@@ -230,3 +230,56 @@ def fitdataMCMC(param,DataPacket,nsteps,guess):
       flat_samples, labels=labels)
 
   return(kappa,vsini,vmac,sampler.flatchain)
+
+
+def fitdata_novsini(param,DataPacket,guess):
+  '''
+  This function fits a set of LSD profiles using scipy's curve fit function.
+
+  Inputs:
+    param - input parameter dictionary
+    DataPacket - input DataPacket with real data
+    guess - array of guess values for kappa and vmac. Ex: np.array([1.3,30])
+
+   Outputs:
+    parameters - array of fit parameters
+    covariance - covariance matrix of the fit
+    modelout - the best fit model
+
+  '''
+  def model(v,kappa,vmac):
+      '''
+      This function creates the line profile model that will be fit to the observed profile
+
+      Inputs:
+        kappa - value of kappa that the walker is on in parameter space
+        vmac - value of vmac that the walker is on in parameter space
+        v - velocity array of the actual data
+
+      Outputs:
+        f - line profile model using the weak field method at the walker's position in parameter space
+      '''
+      param['general']['vmac']=vmac
+      param['general']['logkappa']=np.log(kappa)
+
+      #pyRaven's weakfield disk integration function
+      model=rav.diskint2.analytical(param,False)
+
+      #interpolating the model to be size MCMC wants
+      f=np.interp(v,model['vel'],model['flux'])
+      return(f)
+    
+  param['general']['vsini']=DataPacket.vsini
+
+  x=DataPacket.original.lsds[0].vel-DataPacket.vrad[0]
+  y=DataPacket.original.lsds[0].specI
+  if DataPacket.nobs!=1:
+    for i in range(1,DataPacket.nobs):
+      x=np.append(x,DataPacket.original.lsds[i].vel-DataPacket.vrad[i])
+      y=np.append(y,DataPacket.original.lsds[i].specI)
+
+  parameters,covariance = scipy.optimize.curve_fit(model,x,y,guess)
+
+  modelout=model(x,parameters[0],parameters[1])
+  modelout=modelout[:DataPacket.original.lsds[0].vel.size]
+  return parameters,covariance,modelout
