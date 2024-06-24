151c395351d358c4cf9116c51f409a35df4afe5e
pjstanley1 <pjstanley5391@gmail.com>
2024-06-24 14:49:20 -0400

    Update fitparams.py

--- pyRaven/fitparams.py
+++ pyRaven/fitparams.py
@@ -3,39 +3,43 @@ import scipy
 import copy
 import pyRaven as rav
 
-def fun(theta_,*p):
+def update_params(theta_,param_to_fit,parameters,nobs,guess):
     '''
     This function makes a copy of the param object and updates it based on the input parameters.
     Inputs:
-    :theta_: a list of lists containing parameters to update the param object with. The structure is as follows: [[star1 parameters],[star2 parameters],...]
-    :*p: is a set of additional input parameters, listed below.
-        :fitparam: list of lists of strings indicating which parameters to fit for which stars 
-        :param: list of param objects for each star to fit
-        :DataPacket: the datapacket for the object
-        :guess: a list of lists containing the original guess parameters in the same order and format as theta_
+    :theta_: list of parameter values to update the param objects with
+        :format: [[guess for star 1 parameter to fit,guess for star 1 parameter to fit,...],[guess for star 2 parameter to fit,...]]
+        :Note: MUST BE IN THE SAME ORDER AS param_to_fit. 
+        :Note: If vrad is specified in param_to_fit, the corresponding guess input must be a list with one value per observation, i.e. [vrad1,vrad2,...]
+    :param_to_fit: list of strings stating which parameters should be fit for each star
+        :format: [[star 1 parameter to fit,star 1 parameter to fit,...],[star 2 parameter to fit,...],...]
+        :Note: Order of parameters in arrays doesn't matter, you can fit different parameters for each star. 
+        :Note: Any parameter not stated here will not be fit and will be taken to be its value in the param object 
+    :parameters: list of param objects for each star
+        :format: [param object 1,param object 2,...]
+    :nobs: number of observations
+    :guess: list containing the initial guess values for each parameter to fit and each star. 
+        :format: [[guess for star 1 parameter to fit,guess for star 1 parameter to fit,...],[guess for star 2 parameter to fit,...]]
+        :Note: MUST BE IN THE SAME ORDER AS param_to_fit. 
+        :Note: If vrad is specified in param_to_fit, the corresponding guess input must be a list with one value per observation, i.e. [vrad1,vrad2,...]
 
     Outputs:
     :star: a list of param objects containing the new parameters (in the same format as param)
     
     '''
 
-    fitparam=p[0]
-    param=p[1]
-    DataPacket=p[2]
-    guess=p[3]
-
     star=[]
     prev=0
-    for j in range(len(fitparam)): #loop over stars
+    for j in range(len(param_to_fit)): #loop over stars
         k=0
-        star.append(copy.deepcopy(param[j])) 
-        for i,n in enumerate(fitparam[j]): #loop over parameters that are being fit (skipping vrad for now)
+        star.append(copy.deepcopy(parameters[j])) 
+        for i,n in enumerate(param_to_fit[j]): #loop over parameters that are being fit (skipping vrad for now)
             if n != 'vrad':
                 star[j]['general'][n]=theta_[prev+i+k]
             if n == 'vrad':
-                k=DataPacket.nobs-1
+                k=nobs-1
                 vrads=[]
-                for o in range(DataPacket.nobs):
+                for o in range(nobs):
                     vrads.append(theta_[i+prev+o])
                 star[j]['general']['vrad']=vrads
          
@@ -43,30 +47,36 @@ def fun(theta_,*p):
 
     return(star)
 
-def param_to_model(parameters,DataPacket):
+def param_to_model(parameters,lsds):
     '''
     This function models the multi-star LSD profile given the input list of param objects
 
     Inputs:
-    :parameters: a list of param objects to model
-    :DataPacket: the datapacket for the object
+        :parameters: list of param objects for each star
+            :format: [param object 1,param object 2,...]
+        :lsds: a list containing the arrays of velocity,specI,and specSigI values for the lsd profiles to fit
+            :format: [[velocity array for ob1, '' for ob2, ...],[specI array for ob1, '' for ob2, ...],[specSigI array for ob1, '' for ob2, ...]]
+        
 
-    Outputs:
+    Outputs: 
     :fitmodels: list of stokes I values for the fit model per observation
     '''
-
+    vel=lsds[0]
+    specI=lsds[1]
+    specSigI=lsds[2]
+    nobs=int(len(lsds[0]))
 
     fitmodels=[]
     fys=[]
     for j in range(len(parameters)):
         model=rav.diskint.analytical(parameters[j],False)
-        for o in range(DataPacket.nobs):
-            fys.append(np.interp(DataPacket.scaled.lsds[o].vel,model['vel']+parameters[j]['general']['vrad'][o],model['flux']))
+        for o in range(nobs):
+            fys.append(np.interp(vel[o],model['vel']+parameters[j]['general']['vrad'][o],model['flux']))
     
-    for o in range(DataPacket.nobs):
+    for o in range(nobs):
         obs=np.zeros(len(fys[0]))
         for j in range(len(parameters)):
-            obs+=fys[o+(DataPacket.nobs)*j]
+            obs+=fys[o+(nobs)*j]
         fitmodels.append(obs-j)
     return(fitmodels)
 
@@ -77,49 +87,73 @@ def chi2(theta_,*p):
     Inputs:
     :theta_: list of lists containing the parameters to test
     :*p: is a set of additional input parameters, listed below.
-        :fitparam: list of lists of strings indicating which parameters to fit for which stars 
-        :param: list of param objects for each star to fit
-        :DataPacket: the datapacket for the object
-        :guess: a list of lists containing the original guess parameters in the same order and format as theta_
-
+        :param_to_fit: list of strings stating which parameters should be fit for each star
+            :format: [[star 1 parameter to fit,star 1 parameter to fit,...],[star 2 parameter to fit,...],...]
+            :Note: Order of parameters in arrays doesn't matter, you can fit different parameters for each star. 
+            :Note: Any parameter not stated here will not be fit and will be taken to be its value in the param object 
+        :parameters: list of param objects for each star
+            :format: [param object 1,param object 2,...]
+        :lsds: a list containing the arrays of velocity,specI,and specSigI values for the lsd profiles to fit
+            :format: [[velocity array for ob1, '' for ob2, ...],[specI array for ob1, '' for ob2, ...],[specSigI array for ob1, '' for ob2, ...]]
+        :guess: list containing the initial guess values for each parameter to fit and each star. 
+            :format: [[guess for star 1 parameter to fit,guess for star 1 parameter to fit,...],[guess for star 2 parameter to fit,...]]
+            :Note: MUST BE IN THE SAME ORDER AS param_to_fit. 
+            :Note: If vrad is specified in param_to_fit, the corresponding guess input must be a list with one value per observation, i.e. [vrad1,vrad2,...]
+    
     Outputs:
     :chi2: the chi2 value 
 
     '''
 
+    param_to_fit=p[0]
+    parameters=p[1]
+    lsds=p[2]
+    guess=p[3]
+    nobs=int(len(lsds[0]))
 
     ys=[]
     ys_err=[]
-    DataPacket=p[2]
-    for o in range(DataPacket.nobs):
-        ys.append(DataPacket.scaled.lsds[o].specI)
-        ys_err.append(DataPacket.scaled.lsds[o].specSigI)
 
-    star=fun(theta_,*p)
-    fitmodels=param_to_model(star,DataPacket)
+    for o in range(nobs):
+        ys.append(lsds[1][o])
+        ys_err.append(lsds[2][o])
+
+    star=update_params(theta_,param_to_fit,parameters,nobs,guess)
+    fitmodels=param_to_model(star,lsds)
     
     models=np.hstack(fitmodels) 
     chi2=(np.sum((models-np.hstack(ys))**2/np.hstack(ys_err)))
     return(chi2)
 
-def fitting(param_to_fit,parameters,DataPacket,guess,bounds):
+def fitting(param_to_fit,parameters,lsds,guess,bounds):
     '''
     The actual fitting routine
 
     Inputs:
-    :param_to_fit: list of list of strings stating which parameters should be fit for each star
+    :param_to_fit: list of strings stating which parameters should be fit for each star
+        :format: [[star 1 parameter to fit,star 1 parameter to fit,...],[star 2 parameter to fit,...],...]
+        :Note: Order of parameters in arrays doesn't matter, you can fit different parameters for each star. 
+        :Note: Any parameter not stated here will not be fit and will be taken to be its value in the param object
     :parameters: list of param objects for each star
-    :DataPacket: datapacket for the object
-    :guess: list of lists containing the initial guess values for each parameter to fit and each star
-    :bounds: list of lists of tuples containing the bounds to use for each parameter to fit
-
+        :format: [param object 1,param object 2,...]
+    :lsds: a list containing the arrays of velocity,specI,and specSigI values for the lsd profiles to fit
+        :format: [[velocity array for ob1, '' for ob2, ...],[specI array for ob1, '' for ob2, ...],[specSigI array for ob1, '' for ob2, ...]]
+    :guess: list containing the initial guess values for each parameter to fit and each star. 
+        :format: [[guess for star 1 parameter to fit,guess for star 1 parameter to fit,...],[guess for star 2 parameter to fit,...]]
+        :Note: MUST BE IN THE SAME ORDER AS param_to_fit. 
+        :Note: If vrad is specified in param_to_fit, the corresponding guess input must be a list with one value per observation, i.e. [vrad1,vrad2,...]
+    :bounds: list of tuples containing the bounds to use for each parameter to fit
+        :format: [[(lower bound for star 1 guess,upper bound for star 1 guess),...],[(lower bound for star 2 guess,upper bound for star 2 guess)]]
+        :Note: MUST BE IN THE SAME ORDER AS param_to_fit
+        :Note: If vrad is specified in param_to_fit, the corresponding bounds must be a list with one tuple per observation, i.e. [(lower 1,upper 1),(lower 2, upper 2),...]
+ 
     Outputs:
     :res: the output from scipy.optimize.minimize
     :star: list of param objects for each star containing the final fit values
 
     '''
 
-
+    nobs=int(len(lsds[0]))
     bound=[]
     guesses=np.array([])
     for j in range(len(param_to_fit)):
@@ -128,11 +162,11 @@ def fitting(param_to_fit,parameters,DataPacket,guess,bounds):
             if n!='vrad':
                 bound.append(bounds[j][i])
             if n=='vrad':
-                for o in range(DataPacket.nobs):
+                for o in range(nobs):
                     bound.append(bounds[j][np.where(np.array(param_to_fit[j])=='vrad')[0][0]][o])
 
-    res=scipy.optimize.minimize(chi2,guesses,args=(param_to_fit,parameters,DataPacket,guess),bounds=bound)
+    res=scipy.optimize.minimize(chi2,guesses,args=(param_to_fit,parameters,lsds,guess),bounds=bound)
 
-    star=fun(res.x,param_to_fit,parameters,DataPacket,guess)
+    star=update_params(res.x,param_to_fit,parameters,nobs,guess)
     return(res,star)
 
